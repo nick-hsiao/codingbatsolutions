68dd084cccfcc1db6ac966f67fb7749256b09372
nick-hsiao <47337844+nick-hsiao@users.noreply.github.com>
2019-08-03 07:34:55 -0700

    Update lucky13.java

--- lucky13.java
+++ lucky13.java
@@ -1,10 +1,10 @@
 //Array-2 > lucky13
 //Given an array of ints, return true if the array contains no 1's and no 3's.
 public boolean lucky13(int[] nums) {
-  boolean one = true, three = true;
   for (int num: nums){
-    if (num == 1){ one = false;}
-    if (num == 3){ three = false;}
+    if (num == 1 || num == 3){
+      return false;
+    }
   }
-  return one && three;
+  return true;
 }
