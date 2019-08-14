08771df610d692c4b5a35f3a10586bc31c345ce5
nick-hsiao <hsiao.nicholas@gmail.com>
2019-08-14 15:21:52 -0700

    sum13

---
+++ array2/sum13.java
@@ -0,0 +1,20 @@
+//Array-2 > sum13 
+//Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+
+public int sum13(int[] nums) {
+  int sum = 0;
+  boolean flag = false;
+  for (int i=0; i<nums.length; i++){
+    if (flag) {
+      flag = false;
+      continue;
+    }
+    if (nums[i] != 13){
+      sum += nums[i];
+    }
+    else{
+      flag = true;
+    }
+  }
+  return sum;
+}
