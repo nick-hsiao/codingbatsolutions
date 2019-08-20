27b269481ab6ac62e57115c69707726593e5c3dd
Nick Hsiao <hsiao.nicholas@gmail.com>
2019-08-20 14:11:13 -0700

    countHi

---
+++ recursion1/countHi.java
@@ -0,0 +1,13 @@
+Recursion-1 > countHi
+
+Given a string, compute recursively (no loops) the number of times lowercase "hi" appears in the string.
+
+public int countHi(String str) {
+  if (str.length() < 2){
+    return 0;
+  }
+  if (str.substring(0,2).equals("hi")){
+    return 1 + countHi(str.substring(2));
+  }
+  return countHi(str.substring(1));
+}
