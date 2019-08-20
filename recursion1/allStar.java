235193fc6eb64f1ccd2c26559619f6aefbdc0d18
Nick Hsiao <hsiao.nicholas@gmail.com>
2019-08-20 14:26:51 -0700

    allStar

---
+++ recursion1/allStar.java
@@ -0,0 +1,10 @@
+//Recursion-1 > allStar
+
+//Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+
+public String allStar(String str) {
+  if (str.length() < 2){
+    return str;
+  }
+  return str.charAt(0) + "*" + allStar(str.substring(1));
+}
