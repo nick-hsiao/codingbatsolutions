f6947139a6749e81cb249e03e889fea13a55d3aa
Nick Hsiao <hsiao.nicholas@gmail.com>
2019-08-21 12:51:32 -0700

    countX

---
+++ recursion1/countX.java
@@ -0,0 +1,13 @@
+//Recursion-1 > countX 
+
+//Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
+
+public int countX(String str) {
+  if (str.length() < 1){
+    return 0;
+  }
+  if (str.charAt(0) == 'x'){
+    return 1 + countX(str.substring(1));
+  }
+  return countX(str.substring(1));
+}
